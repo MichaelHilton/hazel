90b74cb17d9c2167de34eee49aa84032d4e398cb
Michael Hilton <michael.hilton@gmail.com>
2017-04-17 21:56:53 -0700

    added push script

---
+++ push.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git clone https://github.com/MichaelHilton/hazelgrove.github.io.git
+  cd hazelgrove.github.io/
+  cp -r ../src/www/ ./
+  cd www
+  git add .
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  #git remote add origin-pages https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
+  git push --quiet
+}
+
+setup_git
+commit_website_files
+upload_files
