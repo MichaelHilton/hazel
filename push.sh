7d8fb9fd2b6162aeeee5d4521c2001b7ef4c9e52
Michael Hilton <michael.hilton@gmail.com>
2017-04-18 16:45:50 -0700

    Update push.sh

--- push.sh
+++ push.sh
@@ -15,6 +15,20 @@ commit_website_files() {
 }
 
 upload_files() {
+  
+  # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+  ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+  ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+  ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+  ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+  openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in github_deploy_key.enc -out deploy_key -d
+  chmod 600 deploy_key
+  eval `ssh-agent -s`
+  ssh-add deploy_key
+
+  # Now that we're all set up, we can push.
+  #git push $SSH_REPO $TARGET_BRANCH
+  
   #git remote add origin-pages https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
    #https://github.com/MichaelHilton/hazelgrove.github.io.git
    git push origin master  
